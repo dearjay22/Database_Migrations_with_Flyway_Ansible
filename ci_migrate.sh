5fa280400f4b65f77f74b6cc4d6002c93f171bef
Patel Jay <52645319+dearjay22@users.noreply.github.com>
2025-11-12 00:36:11 +0000

    changes

--- ci_migrate.sh
+++ ci_migrate.sh
@@ -5,49 +5,48 @@ set -e
 MYSQL_CONTAINER_NAME="a4-mysql"
 MYSQL_ROOT_PASSWORD="rootpass"
 DB_NAME="subscriptions"
-DB_USER="root"
-DB_PASS="rootpass"
-MYSQL_HOST="127.0.0.1"
-MYSQL_PORT="3306"
+DB_USER="sub_user"
+DB_PASS="sub_pass"
+MYSQL_PORT=3306
 
-# Paths
+# Paths for migrations
 INITIAL_MIGRATIONS="./flyway/migrations_initial"
 INCREMENTAL_MIGRATIONS="./flyway/migrations_incremental"
 
 echo "Ensuring MySQL container is running..."
-docker ps --filter "name=$MYSQL_CONTAINER_NAME" --filter "status=running" --format "{{.Names}}" | grep -q $MYSQL_CONTAINER_NAME \
-  || docker run -d --name $MYSQL_CONTAINER_NAME \
-        -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
-        -e MYSQL_DATABASE=$DB_NAME \
-        -p $MYSQL_PORT:3306 \
+docker ps --filter "name=${MYSQL_CONTAINER_NAME}" --filter "status=running" --format "{{.Names}}" | grep -q "${MYSQL_CONTAINER_NAME}" \
+    || docker run -d --name ${MYSQL_CONTAINER_NAME} \
+        -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+        -e MYSQL_DATABASE=${DB_NAME} \
+        -p ${MYSQL_PORT}:3306 \
         mysql:8
 
 echo "Waiting for MySQL to be ready..."
-until docker exec $MYSQL_CONTAINER_NAME mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "SELECT 1" >/dev/null 2>&1; do
-  sleep 3
+until docker exec ${MYSQL_CONTAINER_NAME} mysqladmin ping -h "localhost" -p${MYSQL_ROOT_PASSWORD} --silent; do
+    sleep 3
 done
 echo "MySQL is ready!"
 
+# Create DB user
+echo "Creating DB user with default authentication..."
+docker exec -i ${MYSQL_CONTAINER_NAME} mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "
+    DROP USER IF EXISTS '${DB_USER}'@'%';
+    CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+    GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+    FLUSH PRIVILEGES;"
+
+# Run initial Flyway migrations
 echo "Running initial Flyway migrations..."
-docker run --rm \
-  -v "$(pwd)/flyway/migrations_initial:/flyway/migrations_initial" \
-  --network host \
-  flyway/flyway:10 \
-  -url=jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/$DB_NAME?allowPublicKeyRetrieval=true&useSSL=false \
-  -user=$DB_USER \
-  -password=$DB_PASS \
-  -locations=filesystem:/flyway/migrations_initial \
-  migrate
+docker run --rm -v $(pwd)/flyway:/flyway/sql --network host flyway/flyway:10 \
+    -url=jdbc:mysql://127.0.0.1:3306/${DB_NAME}?allowPublicKeyRetrieval=true&useSSL=false \
+    -user=root -password=${MYSQL_ROOT_PASSWORD} \
+    -locations=filesystem:/flyway/migrations_initial migrate
 
+# Run incremental Flyway migrations
 echo "Running incremental Flyway migrations..."
-docker run --rm \
-  -v "$(pwd)/flyway/migrations_incremental:/flyway/migrations_incremental" \
-  --network host \
-  flyway/flyway:10 \
-  -url=jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/$DB_NAME?allowPublicKeyRetrieval=true&useSSL=false \
-  -user=$DB_USER \
-  -password=$DB_PASS \
-  -locations=filesystem:/flyway/migrations_incremental \
-  migrate
+docker run --rm -v $(pwd)/flyway:/flyway/sql --network host flyway/flyway:10 \
+    -url=jdbc:mysql://127.0.0.1:3306/${DB_NAME}?allowPublicKeyRetrieval=true&useSSL=false \
+    -user=root -password=${MYSQL_ROOT_PASSWORD} \
+    -locations=filesystem:/flyway/migrations_incremental migrate
 
-echo "All Flyway migrations completed!"
+echo "Flyway migrations completed!"
